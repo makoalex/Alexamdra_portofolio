30f1b9f9c38a0ea12d5286a58643f8cd805807ab
makoalex <macovei.a2@gmail.com>
2023-11-01 10:13:49 +0100

    [fix] redoing the services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -21,37 +21,37 @@ const services = [
 export default function Services() {
   return (
     <section className="section" id="services">
-      <div className="container mx-auto">
-        <div className="flex  flex-col lg:flex-row ">
-          <div className=" flex-1 items-end lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0 ">
-            <h2 className=" h2 text-accent mb-6"> What I do</h2>
-            <h3 className="h3 max-w-[450px] mb-16">
-              {" "}
-              I'm a fullstack Javascript developer, with a passion for
-              Front-end.{" "}
-            </h3>
-            <button className="button-header btn-sm btn">See my work</button>
-          </div>
-          {/* services  */}
-          <div className="flex-1">
-            {/* services  */}
-            <div>
+        <div className=" container mx-auto">
+          <div className="flex flex-col lg:flex-row">
+            {/* text */}
+            <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+              <h2 className=" h2 text-accent mb-6">What i do</h2>
+              <h3 className="h3  max-w-[455px] mb-16">   I'm a fullstack Javascript developer, with a passion for
+              Front-end.</h3>
+              <button className="button-header btn-sm btn">See my work</button>
+            </div>
+             {/* services */}
+             <div className="flex-1">
+              {/* service list */}
+              <div>
               {services.map((service, index) => {
-                // destructure service
-                const {name, description, link} = service
-                return (<div key={index}>
-                  <div className=" max-w[470px]">
-                    <h4>{name}</h4>
-                    <p>{description}</p>
+                // destructure service object
+                const { name, description, link } = service;
+                return(
+                  <div key={index}>
+                    <div>
+                      <h4>{name}</h4>
+                      <p>{description}</p>
+                    </div>
+                    <div>Links</div>
                   </div>
-                  <div> {link}</div>
-                </div>
                 )
               })}
-            </div>
+              </div>
+             </div>
           </div>
+
         </div>
-      </div>
     </section>
   );
 }
