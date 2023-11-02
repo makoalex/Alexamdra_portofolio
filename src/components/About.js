144b05baaf2d4ebc06ba2c6439da02867731a72b
makoalex <macovei.a2@gmail.com>
2023-11-02 20:15:21 +0100

    [fix] adding framer motion to the about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,51 +1,68 @@
-import React from 'react';
-import { useInView } from 'react-intersection-observer';
-import CountUp from 'react-countup';
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import CountUp from "react-countup";
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
 
 export default function About() {
   const [ref, inVew] = useInView({ threshold: 0.5 });
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div className='flex flex-col gap-y-6 lg:flex-row lg:items-center lg:gap-x-32 lg:gap-y-0 h-screen'>
-          <div className="flex-1 mr-11 bg-about bg-contain bg-no-repeat h-[400px] mix-blend-lighten bg-top"></div>
-          <div className='flex-1'>
-            <h2 className='h2 text-accent '>About me</h2>
-            <h3 className='h3 mb-3'>
+        <div className="flex flex-col gap-y-6 lg:flex-row lg:items-center lg:gap-x-32 lg:gap-y-0 h-screen">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+           className="flex-1 mr-11 bg-about bg-contain bg-no-repeat h-[400px] mix-blend-lighten bg-top"></motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent ">About me</h2>
+            <h3 className="h3 mb-3">
               I'm a fullstack Javascript developer, with a passion for
-              Front-end.{' '}
+              Front-end.{" "}
             </h3>
-            <p className='mb-6'>
+            <p className="mb-6">
               Using JavaScript for both front-end and back-end development,
               creating dynamic and cohesive web applications that seamlessly
-              integrate user interfaces, server logic, and databases. 
+              integrate user interfaces, server logic, and databases.
             </p>
-            <div className=' flex gap-x-6 lg:gap-x-10 mb-12'>
+            <div className=" flex gap-x-6 lg:gap-x-10 mb-12">
               <div>
-                <div className='font-[40px] font-tertiary text-gradient'>
-                  {inVew? <CountUp start={0} end={15} duration={5} /> :null}
+                <div className="font-[40px] font-tertiary text-gradient">
+                  {inVew ? <CountUp start={0} end={15} duration={5} /> : null}
                 </div>
-                <div className=' font-secondary text-sm tracking-[2px]'>
+                <div className=" font-secondary text-sm tracking-[2px]">
                   Git <br />
-                Repositories
+                  Repositories
                 </div>
               </div>
               <div>
-                <div className='font-[40px] font-tertiary text-gradient'>
-                  {inVew? <CountUp start={0} end={5} duration={3} /> :null}
+                <div className="font-[40px] font-tertiary text-gradient">
+                  {inVew ? <CountUp start={0} end={5} duration={3} /> : null}
                 </div>
-                <div className=' font-secondary text-sm tracking-[2px]'>
+                <div className=" font-secondary text-sm tracking-[2px]">
                   Bootcamps <br />
-                Completed
+                  Completed
                 </div>
               </div>
             </div>
-            <div className='flex gap-8 items-center'>
-            <button className='button-header btn-sm btn  '>Get in touch</button>
-            <a href='#' className='text-gradient btn-link'>My Portofolio</a>
+            <div className="flex gap-8 items-center">
+              <button className="button-header btn-sm btn  ">
+                Get in touch
+              </button>
+              <a href="#" className="text-gradient btn-link">
+                My Portofolio
+              </a>
             </div>
-          </div>
-          
+          </motion.div>
         </div>
       </div>
     </section>
