a206596f3e771abcdb29a3cc5255e17aff7b79f0
makoalex <macovei.a2@gmail.com>
2023-11-18 12:37:26 +0100

    [feat] adding latest project

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,7 @@ import { fadeIn } from "../variants";
 import Img1 from "../assets/Image1.png";
 import Img2 from "../assets/img2.png";
 import Img3 from "../assets/Group 3.png";
+import Img4 from "../assets/Salt.png";
 
 export default function Work() {
   return (
@@ -31,6 +32,7 @@ export default function Work() {
               </p>
             </div>
             {/** image */}
+            <a href="https://pendel-hub.vercel.app/">
             <div className="group relative lg:top-14 overflow-hidden border-2 lg:max-w-[575px] lg:max-h-[400px] mt-12 border-white/60 rounded-xl">
               {/* overlay */}
               <div className="group-hover:bg-black/30 w-full h-full absolute z-40 transition-all duration-300"></div>
@@ -49,6 +51,7 @@ export default function Work() {
                 <span className="text-3xl text-white">Pendel Hub</span>
               </div>
             </div>
+            </a>
           </motion.div>
           <motion.div
             variants={fadeIn("left", 0.2)}
@@ -76,6 +79,7 @@ export default function Work() {
                 <span className="text-3xl text-white">Reserve Eats</span>
               </div>
             </div>
+            <a href="https://github.com/SaltyUltraCrepidarian/EcoHarbor">
             <div className="group relative overflow-hidden border-2 border-white/60 rounded-xl">
               {/* overlay */}
               <div className="group-hover:bg-black/30 w-full h-full absolute z-40 transition-all duration-300"></div>
@@ -94,6 +98,27 @@ export default function Work() {
                 <span className="text-3xl text-white">EcoHarbour</span>
               </div>
             </div>
+            </a>
+            <a href="https://talents.salt.dev/">
+            <div className="group relative overflow-hidden border-2 border-white/60 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/30 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                src={Img4}
+                alt="project image 1"
+                className=" group-hover:scale-150 transition-all group-hover:rotate-12 duration-500"
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient "> Next.js/ UI/UX design/</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Salt Talent Pool</span>
+              </div>
+            </div>
+            </a>
           </motion.div>
         </div>
       </div>
