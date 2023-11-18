6feefbdd3d9ad7801a54bf226873beeaf1bca266
makoalex <macovei.a2@gmail.com>
2023-11-18 19:56:55 +0100

    [feat] adding skills component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Services from './components/Services';
 import Work from './components/Work';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Services />
       <Work />
+      <Skills/>
       <Contact />
     </div>
   );
---
+++ src/components/Skills.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeInAnimationVariants } from "../variants";
+
+const skills = [
+  "Html",
+  "CSS",
+  "Javascript",
+  "TypeScript",
+  "React.js",
+  "Next.js",
+  "Tailwind",
+  "Bootstrap",
+  "Sass",
+  "Figma",
+  "Frame Motion",
+  "Express",
+  "Node.js",
+  "Prisma",
+  "Git",
+  "MongoDb",
+  "PostgreSQl",
+];
+
+export default function Skills() {
+  return (
+    <div
+      className="section flex flex-1 justify-center items-center w-screen -mb-20 mt-40 text-center"
+      id="skills"
+    >
+      <div className="container mx-auto">
+        <h2 className="h2 leading-tight text-accent p-4 ">My Skills</h2>
+        <ul className="flex justify-center items-center flex-wrap gap-4 text-lg font-primary font-light">
+          {skills.map((skill, index) => (
+            <motion.li
+              className=" border rounded-xl flex flex-col px-5 py-3 "
+              key={index}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ once: true }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
